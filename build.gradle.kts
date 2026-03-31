3cc8159b85933423383feb517137333c9a5f7de5
Booze <61169560+ThatBooze@users.noreply.github.com>
2026-03-31 14:18:01 -0400

    Clean up build.gradle.kts
    Yuck

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,7 @@
+@file:Suppress("SpellCheckingInspection")
+
 plugins {
 	id("net.fabricmc.fabric-loom-remap")
-	`maven-publish`
 }
 
 version = providers.gradleProperty("mod_version").get()
@@ -11,21 +12,16 @@ base {
 }
 
 repositories {
-	// Add repositories to retrieve artifacts from in here.
-	// You should only use this when depending on other mods because
-	// Loom adds the essential maven repositories to download Minecraft and libraries from automatically.
-	// See https://docs.gradle.org/current/userguide/declaring_repositories.html
-	// for more information about repositories.
+	maven {
+			name = "ParchmentMC"
+			url = uri("https://maven.parchmentmc.org")
+		}
 	maven {
 		name = "Figura Maven"
 		url = uri("https://maven.figuramc.org/snapshots")
 		content {
 			includeGroup("org.figuramc")
 		}
-	}
-	maven {
-		name = "ParchmentMC"
-		url = uri("https://maven.parchmentmc.org")
 	}
 }
 
@@ -36,11 +32,10 @@ fabricApi {
 }
 
 dependencies {
-	// To change the versions see the gradle.properties file
 	minecraft("com.mojang:minecraft:${providers.gradleProperty("minecraft_version").get()}")
-	modImplementation("net.fabricmc:fabric-loader:${providers.gradleProperty("loader_version").get()}")
 	mappings(loom.officialMojangMappings())
-	// Fabric API. This is technically optional, but you probably want it anyway.
+
+	modImplementation("net.fabricmc:fabric-loader:${providers.gradleProperty("loader_version").get()}")
 	modImplementation("net.fabricmc.fabric-api:fabric-api:${providers.gradleProperty("fabric_api_version").get()}")
 
 	modCompileOnly("org.figuramc:figura-fabric:${providers.gradleProperty("figura_version").get()}")
@@ -55,17 +50,15 @@ tasks.processResources {
 }
 
 tasks.withType<JavaCompile>().configureEach {
-	options.release = 17
+	options.release = 21
 }
 
 java {
-	// Loom will automatically attach sourcesJar to a RemapSourcesJar task and to the "build" task
-	// if it is present.
-	// If you remove this line, sources will not be generated.
 	withSourcesJar()
 
-	sourceCompatibility = JavaVersion.VERSION_17
-	targetCompatibility = JavaVersion.VERSION_17
+	toolchain {
+		languageVersion.set(JavaLanguageVersion.of(21))
+	}
 }
 
 tasks.jar {
@@ -78,22 +71,4 @@ tasks.jar {
 
 tasks.named("runDatagen") {
 	outputs.dir("src/main/generated")
-}
-
-// configure the maven publication
-publishing {
-	publications {
-		register<MavenPublication>("mavenJava") {
-			artifactId = base.archivesName.get()
-			from(components["java"])
-		}
-	}
-
-	// See https://docs.gradle.org/current/userguide/publishing_maven.html for information on how to set up publishing.
-	repositories {
-		// Add repositories to publish to here.
-		// Notice: This block does NOT have the same function as the block in the top level.
-		// The repositories here will be used for publishing your artifact, not for
-		// retrieving dependencies.
-	}
 }
